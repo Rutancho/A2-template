601c96fccca1395a73f9ed0211fcd053e2a167e6
jheo9 <jheo9@myseneca.ca>
2024-02-16 00:30:51 -0500

    first commit

---
+++ modules/legoSets.js
@@ -0,0 +1,74 @@
+/********************************************************************************
+*  WEB322 – Assignment 03
+* 
+*  I declare that this assignment is my own work in accordance with Seneca's
+*  Academic Integrity Policy:
+* 
+*  https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
+* 
+*  Name: Jaehyuk Heo Student ID: 105421234 Date: 2024-02-15
+*
+*  Published URL: ___________________________________________________________
+*
+********************************************************************************/
+
+
+const setData = require("../data/setData");
+const themeData = require("../data/themeData");
+
+let sets = [];
+
+function initialize() {
+  return new Promise((resolve, reject) => {
+    setData.forEach(setElement => {
+      const theme = themeData.find(themeElement => themeElement.id == setElement.theme_id);
+      if (theme) {
+        let setWithTheme = { ...setElement, theme: theme.name };
+        sets.push(setWithTheme);
+      } 
+      else 
+      {
+      reject(new Error("Theme not found for set: " + setElement.id));
+      }
+    });
+    resolve();
+  });
+}
+
+function getAllSets() {
+  return new Promise((resolve, reject) => {
+    resolve(sets);
+  });
+}
+
+function getSetByNum(setNum) {
+
+  return new Promise((resolve, reject) => {
+    let foundSet = sets.find(s => s.set_num == setNum);
+
+    if (foundSet) {
+      resolve(foundSet)
+    } else {
+      reject("Unable to find requested set");
+    }
+
+  });
+
+}
+
+function getSetsByTheme(theme) {
+
+  return new Promise((resolve, reject) => {
+    let foundSets = sets.filter(s => s.theme.toUpperCase().includes(theme.toUpperCase()));
+
+    if (foundSets.length > 0) {
+      resolve(foundSets)
+    } else {
+      reject("Unable to find requested sets");
+    }
+
+  });
+
+}
+
+module.exports = { initialize, getAllSets, getSetByNum, getSetsByTheme }
---
+++ server.js
@@ -0,0 +1,67 @@
+/********************************************************************************
+*  WEB322 – Assignment 02
+* 
+*  I declare that this assignment is my own work in accordance with Seneca's
+*  Academic Integrity Policy:
+* 
+*  https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
+* 
+*  Name: Jaehyuk Heo Student ID: 105421234 Date: 2024-01-31
+*
+********************************************************************************/
+
+
+
+const express = require('express');
+const legoData = require('./modules/legoSets');
+const path = require('path');
+
+const app =express();
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+  });
+  
+
+  app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/about.html"));
+  });
+
+  app.get("/lego/sets", async (req, res) => {
+    try {
+      console.log("첫번")
+      let sets = req.query.theme ? await legoData.getSetsByTheme(req.query.theme) : await legoData.getAllSets();
+      console.log("두번")
+      res.send(sets);
+    } catch (err) {
+      console.log("삼번")
+      res.status(404).send('Unable to find requested sets');
+    }
+  });
+
+
+app.get("/lego/sets/:num", async (req, res) => {
+    try {
+      console.log("사번")
+      let set = await legoData.getSetByNum(req.params.num);
+      console.log("6번")
+      res.send(set);
+    } catch (err) {
+      console.log("7번")
+      res.status(404).send('Set not found');
+      console.log("9번")
+    }
+  });
+  
+
+  app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
+  });
+  
+  legoData.initialize().then(() => {
+    app.listen(HTTP_PORT, () => console.log(`Server listening on: ${HTTP_PORT}`));
+  });
